9651724df70493cd5c4c9a0b99a40b10f28f469e
Jan David <jandavid@6a64.com>
2020-02-17 21:56:03 +0100

    Set up entrypoint for library
    The `F1` struct is the intended entrypoint for consumers of the library.
    It is a high-level interface that coordinates and hides the library's
    low-level internals.

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,15 @@
 // Documentation is required for all public items to ensure that this library
 // can easily be used by other developers.
 #![deny(missing_docs)]
+
+/// A high-level interface to the telemetry data of modern F1 video games.
+///
+/// The F1 struct implements a high-level interface to the telemetry data of the
+/// modern F1 video games. It is the recommended way to use the library, as it
+/// provides a simple interface to consumers that hides the low-level internals
+/// of the library.
+///
+/// TODO Add usage example
+pub struct F1 {}
+
+impl F1 {}
