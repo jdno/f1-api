cfd42c6546f9a734b52565c565847cebeaf9e054
Jan David <jandavid@hellobits.dev>
2020-03-05 19:32:26 +0100

    Decode car status packet

--- src/nineteen/status.rs
+++ src/nineteen/status.rs
@@ -1,4 +1,8 @@
 use crate::nineteen::{Flag, PacketHeader};
+use crate::packet::FromBytes;
+use bitflags::_core::convert::TryFrom;
+use bytes::{Buf, BytesMut};
+use std::io::{Cursor, Error, ErrorKind};
 
 pub enum TractionControl {
     Off = 0,
@@ -142,4 +146,175 @@ pub struct CarStatusPacket {
 
     /// The status of each car in the session.
     pub car_status: Vec<CarStatus>,
+}
+
+impl TryFrom<u8> for TractionControl {
+    type Error = Error;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(TractionControl::Off),
+            1 => Ok(TractionControl::Low),
+            2 => Ok(TractionControl::High),
+            _ => Err(Error::new(
+                ErrorKind::InvalidData,
+                "Failed to decode transaction control.",
+            )),
+        }
+    }
+}
+
+impl TryFrom<u8> for FuelMix {
+    type Error = Error;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(FuelMix::Lean),
+            1 => Ok(FuelMix::Standard),
+            2 => Ok(FuelMix::Rich),
+            3 => Ok(FuelMix::Max),
+            _ => Err(Error::new(
+                ErrorKind::InvalidData,
+                "Failed to decode fuel mix.",
+            )),
+        }
+    }
+}
+
+impl TryFrom<i8> for DrsStatus {
+    type Error = Error;
+
+    fn try_from(value: i8) -> Result<Self, Self::Error> {
+        match value {
+            -1 => Ok(DrsStatus::Unknown),
+            0 => Ok(DrsStatus::NotAllowed),
+            1 => Ok(DrsStatus::Allowed),
+            _ => Err(Error::new(
+                ErrorKind::InvalidData,
+                "Failed to decode DRS status.",
+            )),
+        }
+    }
+}
+
+impl TryFrom<u8> for TyreCompound {
+    type Error = Error;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            7 => Ok(TyreCompound::F1Intermediate),
+            8 => Ok(TyreCompound::F1Wet),
+            9 => Ok(TyreCompound::ClassicDry),
+            10 => Ok(TyreCompound::ClassicWet),
+            11 => Ok(TyreCompound::F2SuperSoft),
+            12 => Ok(TyreCompound::F2Soft),
+            13 => Ok(TyreCompound::F2Medium),
+            14 => Ok(TyreCompound::F2Hard),
+            15 => Ok(TyreCompound::F2Wet),
+            16 => Ok(TyreCompound::F1C5),
+            17 => Ok(TyreCompound::F1C4),
+            18 => Ok(TyreCompound::F1C3),
+            19 => Ok(TyreCompound::F1C2),
+            20 => Ok(TyreCompound::F1C1),
+            _ => Err(Error::new(
+                ErrorKind::InvalidData,
+                "Failed to decode tyre compound.",
+            )),
+        }
+    }
+}
+
+impl TryFrom<u8> for VisualCompound {
+    type Error = Error;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            7 => Ok(VisualCompound::Intermediate),
+            8 => Ok(VisualCompound::Wet),
+            16 => Ok(VisualCompound::Soft),
+            17 => Ok(VisualCompound::Medium),
+            18 => Ok(VisualCompound::Hard),
+            _ => Err(Error::new(
+                ErrorKind::InvalidData,
+                "Failed to decode visual tyre compound.",
+            )),
+        }
+    }
+}
+
+impl TryFrom<u8> for ErsDeployMode {
+    type Error = Error;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0 => Ok(ErsDeployMode::None),
+            1 => Ok(ErsDeployMode::Low),
+            2 => Ok(ErsDeployMode::Medium),
+            3 => Ok(ErsDeployMode::High),
+            4 => Ok(ErsDeployMode::Overtake),
+            5 => Ok(ErsDeployMode::Hotlap),
+            _ => Err(Error::new(
+                ErrorKind::InvalidData,
+                "Failed to decode ERS deployment mode.",
+            )),
+        }
+    }
+}
+
+impl FromBytes for CarStatusPacket {
+    fn buffer_size() -> usize {
+        1143
+    }
+
+    fn decode(cursor: &mut Cursor<BytesMut>) -> Result<Self, Error>
+    where
+        Self: Sized,
+    {
+        let header = PacketHeader::decode(cursor)?;
+        let mut car_status = Vec::with_capacity(20);
+
+        for _ in 0..20 {
+            car_status.push(CarStatus {
+                traction_control: TractionControl::try_from(cursor.get_u8())?,
+                abs: cursor.get_u8() > 0,
+                fuel_mix: FuelMix::try_from(cursor.get_u8())?,
+                brake_bias: cursor.get_u8(),
+                pit_limiter: cursor.get_u8() > 0,
+                fuel_remaining: cursor.get_f32_le(),
+                fuel_capacity: cursor.get_f32_le(),
+                fuel_remaining_laps: cursor.get_f32_le(),
+                max_rpm: cursor.get_u16_le(),
+                idle_rpm: cursor.get_u16_le(),
+                gear_count: cursor.get_u8(),
+                drs: DrsStatus::try_from(cursor.get_i8())?,
+                tyre_wear: (
+                    cursor.get_u8(),
+                    cursor.get_u8(),
+                    cursor.get_u8(),
+                    cursor.get_u8(),
+                ),
+                tyre_compound: TyreCompound::try_from(cursor.get_u8())?,
+                visual_compound: VisualCompound::try_from(cursor.get_u8())?,
+                tyre_damage: (
+                    cursor.get_u8(),
+                    cursor.get_u8(),
+                    cursor.get_u8(),
+                    cursor.get_u8(),
+                ),
+                front_left_wing_damage: cursor.get_u8(),
+                front_right_wing_damage: cursor.get_u8(),
+                rear_wing_damage: cursor.get_u8(),
+                engine_damage: cursor.get_u8(),
+                gear_box_damage: cursor.get_u8(),
+                vehicle_flags: Flag::try_from(cursor.get_i8())?,
+                ers_energy: cursor.get_f32_le(),
+                ers_mode: ErsDeployMode::try_from(cursor.get_u8())?,
+                ers_harvest_mguk: cursor.get_f32_le(),
+                ers_harvest_mguh: cursor.get_f32_le(),
+                ers_deployed: cursor.get_f32_le(),
+            });
+        }
+
+        Ok(CarStatusPacket { header, car_status })
+    }
 }
