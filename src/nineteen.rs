b41fe7b03f3139072e8c419c9e8350c49cafe9f0
Jan David <jandavid@hellobits.dev>
2020-03-08 13:04:57 +0100

    Add wrapper around packets of F1 2019
    A wrapper has been implemented that can decode and return any of the
    packets that F1 2019 publishes.

--- src/nineteen.rs
+++ src/nineteen.rs
@@ -1,9 +1,21 @@
 //! API specification for F1 2019.
 
-mod header;
-pub use header::{PacketHeader, PacketType};
+use crate::nineteen::event::EventPacket;
+use crate::nineteen::header::PacketType;
+use crate::nineteen::lap::LapPacket;
+use crate::nineteen::motion::MotionPacket;
+use crate::nineteen::participants::ParticipantsPacket;
+use crate::nineteen::session::SessionPacket;
+use crate::nineteen::setup::CarSetupPacket;
+use crate::nineteen::status::CarStatusPacket;
+use crate::nineteen::telemetry::TelemetryPacket;
+use crate::packet::FromBytes;
+use bytes::{Buf, BytesMut};
 use std::convert::TryFrom;
-use std::io::{Error, ErrorKind};
+use std::io::{Cursor, Error, ErrorKind};
+
+mod header;
+pub use header::PacketHeader;
 
 pub mod event;
 pub mod lap;
@@ -23,6 +35,17 @@ pub enum Flag {
     Red = 4,
 }
 
+pub enum Packet {
+    Event(EventPacket),
+    Lap(LapPacket),
+    Motion(MotionPacket),
+    Participants(ParticipantsPacket),
+    Session(SessionPacket),
+    Setup(CarSetupPacket),
+    Telemetry(TelemetryPacket),
+    Status(CarStatusPacket),
+}
+
 /// Data for all vehicles is provided as an array. References to the data in
 /// this array are made in the form of a vehicle index.
 pub type VehicleIndex = u8;
@@ -40,5 +63,44 @@ impl TryFrom<i8> for Flag {
             4 => Ok(Flag::Red),
             _ => Err(Error::new(ErrorKind::InvalidData, "Failed to decode flag.")),
         }
+    }
+}
+
+impl Packet {
+    fn peek_packet_id(cursor: &mut Cursor<BytesMut>) -> Result<PacketType, Error> {
+        cursor.set_position(5 as u64);
+
+        let packet_type = PacketType::try_from(cursor.get_u8())?;
+
+        cursor.set_position(0);
+        Ok(packet_type)
+    }
+}
+
+impl FromBytes for Packet {
+    fn buffer_size() -> usize {
+        6
+    }
+
+    fn decode(cursor: &mut Cursor<BytesMut>) -> Result<Self, Error>
+    where
+        Self: Sized,
+    {
+        let packet_type = Packet::peek_packet_id(cursor)?;
+
+        let packet = match packet_type {
+            PacketType::Motion => Packet::Motion(MotionPacket::from_bytes(cursor)?),
+            PacketType::Session => Packet::Session(SessionPacket::from_bytes(cursor)?),
+            PacketType::Lap => Packet::Lap(LapPacket::from_bytes(cursor)?),
+            PacketType::Event => Packet::Event(EventPacket::from_bytes(cursor)?),
+            PacketType::Participants => {
+                Packet::Participants(ParticipantsPacket::from_bytes(cursor)?)
+            }
+            PacketType::Setup => Packet::Setup(CarSetupPacket::from_bytes(cursor)?),
+            PacketType::Telemetry => Packet::Telemetry(TelemetryPacket::from_bytes(cursor)?),
+            PacketType::Status => Packet::Status(CarStatusPacket::from_bytes(cursor)?),
+        };
+
+        Ok(packet)
     }
 }
